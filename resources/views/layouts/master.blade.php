282f88710638cd5da188c9a5005b94ddafadf436
Muhamad Rafli Alfarizqi <70202056+xrafffcode@users.noreply.github.com>
2022-02-19 13:25:14 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://company-rafa.herokuapp.com/css/style.css">
 
     <title>Company Profile</title>
 </head>
@@ -17,7 +17,7 @@
     @yield('content')
     @include('partials.footer')
 
-    <script src="{{ asset('js/script.js') }}"></script>
+    <script src="https://company-rafa.herokuapp.com/js/script.js"></script>
     <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 </body>
 
